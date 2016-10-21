92e228f56a3cbd7060c6f73c7e6feb9028d5ee12
Tam Nguyen <mr.tamron@gmail.com>
2016-10-21 11:31:44 -0700

    add editform for meals

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,15 @@ import { Component } from '@angular/core';
   <div class="wrap">
   <h1>Meal Tracker</h1>
     <div class="container">
-      <div class="pie" *ngFor="let currentMeal of meals">
-        <p>{{currentMeal.mealName}}</p>
+      <h3 (click)="showDetails(currentMeal)" *ngFor="let currentMeal of meals">{{ currentMeal.mealName }}</h3>
+      <div>
+        <h1>Edit Task</h1>
+        <label>Description: {{ selectedMeal.mealName }}</label>
+        <input [(ngModel)]="selectedMeal.mealName">
+        <label>Calories: {{ selectedMeal.calorieInfo }}</label>
+        <input [(ngModel)]="selectedMeal.calorieInfo">
+        <label>Meal Information: {{ selectedMeal.mealInfo }}</label>
+        <input [(ngModel)]="selectedMeal.mealInfo">
       </div>
     </div>
   </div>
@@ -15,16 +22,19 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  public meals: Meal[];
-  constructor() {
-    this.meals = [
+  public meals: Meal[] = [
       new Meal("Don Carlos's Burrito", 800, "This burrito will most definitely fill you up!"),
       new Meal("Stella's Spaghetti", 2000, "Haven't ate all day? This spaghetti will do justice!"),
       new Meal("Sally's Salad", 350, "Salads are cool, but they won't fill you up")
     ];
+
+  selectedMeal: Meal = this.meals[0];
+  showDetails(clickedMeal: Meal) {
+    this.selectedMeal = clickedMeal;
   }
 }
 
 export class Meal {
+  public done: boolean = false;
   constructor(public mealName: string, public calorieInfo: number, public mealInfo: string) {}
 }
